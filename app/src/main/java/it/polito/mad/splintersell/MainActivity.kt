32823a9b3cc92d9f202b940917802ee33c466d64
ACicca <mymusicchannel20@gmail.com>
2020-04-22 16:05:18 +0200

    Drawer Adapt Layouts improvement.

--- app/src/main/java/it/polito/mad/splintersell/MainActivity.kt
+++ app/src/main/java/it/polito/mad/splintersell/MainActivity.kt
@@ -1,7 +1,12 @@
 package it.polito.mad.splintersell
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import android.view.Menu
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,6 +18,15 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.graphics.drawable.RoundedBitmapDrawable
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import it.polito.mad.splintersell.ui.profile_show.EXTRA_EMAIL
+import it.polito.mad.splintersell.ui.profile_show.EXTRA_NICKNAME
+import kotlinx.android.synthetic.main.fragment_show_profile.*
+import kotlinx.android.synthetic.main.nav_header_main.*
+import org.json.JSONObject
+import java.io.File
+import java.io.FileInputStream
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,10 +52,68 @@ class MainActivity : AppCompatActivity() {
             R.id.nav_item_list, R.id.nav_gallery, R.id.nav_slideshow,R.id.nav_item_details,R.id.nav_edit_item, R.id.nav_show_profile), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-    }
+        val headerView = navView.getHeaderView(0)
+
+        retrievePreferencesMain(headerView)
+
+        }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
+
+
+    private fun retrievePreferencesMain(headerView: View) {
+
+        val textViewNick = headerView.findViewById(R.id.nav_profile_name) as TextView
+        val textViewMail = headerView.findViewById(R.id.nav_profile_mail) as TextView
+        val imgView = headerView.findViewById(R.id.imageView) as ImageView
+
+        val sharedPref: SharedPreferences = getPreferences(Context.MODE_PRIVATE) ?: return
+        val profile: String? = sharedPref.getString("Profile", null)
+
+        if (profile != null) {
+
+            val jasonObject = JSONObject(profile)
+            val savedNickname: String
+            val savedEmail: String
+
+
+            savedNickname = if (jasonObject.has(EXTRA_NICKNAME))
+                jasonObject.getString(EXTRA_NICKNAME)
+            else
+                resources.getString(R.string.nick)
+
+            savedEmail = if (jasonObject.has(EXTRA_EMAIL))
+                jasonObject.getString(EXTRA_EMAIL)
+            else
+                resources.getString(R.string.mail)
+
+
+            textViewMail.text = savedEmail
+            textViewNick.text = savedNickname
+
+        }
+
+
+            val filename = "img"
+            val file = File(this.filesDir, filename)
+            val fileExists = file.exists()
+            if (fileExists) {
+                val fis: FileInputStream = openFileInput(filename)
+                val bitmap = BitmapFactory.decodeStream(fis)
+                val roundDrawable: RoundedBitmapDrawable =
+                    RoundedBitmapDrawableFactory.create(resources, bitmap)
+                roundDrawable.isCircular = true
+                fis.close()
+                imgView.setImageDrawable(roundDrawable)
+
+            }
+
+
+
+
+    }
+
 }
--- app/src/main/java/it/polito/mad/splintersell/ui/profile_show/ShowProfile.kt
+++ app/src/main/java/it/polito/mad/splintersell/ui/profile_show/ShowProfile.kt
@@ -48,6 +48,7 @@ class ShowProfile : Fragment() {
 
         this.retrieveImage()
 
+
     }
 
 
