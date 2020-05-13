5452abef4204afd98424a2d06190afb092c0acd6
MFresh <mi21KAS@a95FlV>
2020-05-13 11:28:53 +0200

    Added User data model

---
+++ app/src/main/java/it/polito/mad/splintersell/User.kt
@@ -0,0 +1,10 @@
+package it.polito.mad.splintersell
+
+data class User(
+
+    var fullname: String = "",
+    var nickname: String = "",
+    var email: String = "",
+    var location: String = ""
+
+) {}
